47cf1ee1c67a8b261439ea524134bf5d16b40a74
Brien123 <zbrine1335@gmail.com>
2024-08-09 22:53:36 +0100

    ...

--- eneo/settings.py
+++ eneo/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1j835y+tftc^)jgjsti#zz^3exd6ip)y)!1p%fb%=!hf4x2fr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eneo-mobile.onrender.com']
 
 
 # Application definition
